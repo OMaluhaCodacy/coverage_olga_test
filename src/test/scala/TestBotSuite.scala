5061a2918a0ba33e7481178655c9920c2642c181
olgamaluhacodacy <olga.maluha@codacy.com>
2023-01-03 10:31:13 +0100

    5th commit

--- src/test/scala/TestBotSuite.scala
+++ src/test/scala/TestBotSuite.scala
@@ -6,7 +6,7 @@ class TestBotSuite extends AnyFlatSpec{
     val result = answerUser("Bye")
     assert(result == "See you!")
   }
-  "answerUser" should "return correct answer on hello" in {
+  it should "return correct answer on hello" in {
     val result = answerUser("Hello")
     assert(result == "I'm bot, tell me your problem")
   }
